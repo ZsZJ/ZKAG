9918ab0260b5b70fdc6e9f95d9013979e217aadb
jesseyvanofferen <vanofferenjessey@gmail.com>
2017-11-20 14:27:13 +0100

    install admin template files

---
+++ resources/views/components/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer-->
+<footer class="footer">
+        <span class="pull-right">
+            Version {{ env('APP_VERSION') }}
+        </span>
+    &copy; {{ env('APP_NAME') }} {{ date('Y') }}
+</footer>
